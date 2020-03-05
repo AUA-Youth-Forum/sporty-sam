54d2980595bd0cf97645b9d4db0a49c9dc78bf20
SahanDisa <t.sahan998@gmail.com>
2020-03-05 10:30:42 +0530

    Add demo splash screen

--- sporty_sam/lib/home.dart
+++ sporty_sam/lib/home.dart
@@ -1,35 +1,36 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            Text(
-              'Sam',
-              style: Theme.of(context).textTheme.display1,
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add_box),
+              tooltip: 'Show snackbar',
+              onPressed: (){},
             ),
+            IconButton(
+              icon: Icon(Icons.close),
+              tooltip: 'Close the app',
+              onPressed: (){
+                Navigator.pop(context);
+              },
+            )
           ],
+
+        ),
+        body: Center(
+          child: Text(
+            'This is the home page of sporty sam',
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
-
     );
   }
 }
