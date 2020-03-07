c86be3ec94dbd438999f005c7b4acd8febe6f3c0
georgeraveen <georgeraveen@gmail.com>
2020-03-07 14:15:03 +0530

    petshop and teddy home

--- sporty_sam/lib/home.dart
+++ sporty_sam/lib/home.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:sporty_sam/petshop.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -13,10 +14,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
+//      appBar: AppBar(
+//
+//        title: Text(widget.title),
+//      ),
       body: Center(
 
         child: Column(
@@ -28,17 +29,22 @@ class _MyHomePageState extends State<MyHomePage> {
               'Sam',
               style: Theme.of(context).textTheme.display1,
             ),
-            Container(
-              height: 200,
-              width: 200,
+            InkWell(
+              child: Container(
+              height: 300,
+              width: 300,
               child: FlareActor("lib/assets/animations/teddy.flr",
                   animation: "success",
                   //color: Colors.red
               ),
               
             ),
-            
-          ],
+            onTap: (){
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context)=>PetShop(),
+                ));
+            },
+            )],
 
         ),
       ),
