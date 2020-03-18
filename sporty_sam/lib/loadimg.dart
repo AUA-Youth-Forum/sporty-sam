b0f3bf4c36be09abd40be893aa5b51494730814e
georgeraveen <georgeraveen@gmail.com>
2020-03-18 11:12:35 +0530

    update firebase image load

--- sporty_sam/lib/loadimg.dart
+++ sporty_sam/lib/loadimg.dart
@@ -5,6 +5,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+import 'package:cached_network_image/cached_network_image.dart';
+
 class LoadImg extends StatefulWidget {
   LoadImg({this.fileName});
 
@@ -15,34 +17,73 @@ class LoadImg extends StatefulWidget {
 }
 
 class _LoadImg extends State<LoadImg> {
+  String imgUrl;
+  String data;
   Uint8List imageFile;
   StorageReference cardImgReference =
       FirebaseStorage.instance.ref().child("healthtips");
+
+//  @override
+//  Widget build(BuildContext context) {
+//    print('zzzz');
+//
+//    const int MAXSIZE = 4 * 1024 * 1024;
+//    cardImgReference
+//        .child('${widget.fileName}.jpg')
+//        .getData(MAXSIZE)
+//        .then((data) {
+////      imageFile = data;
+//      print(widget.fileName);
+//      this.setState(() {
+//        imageFile = data;
+//      });
+//    }
+//    ).catchError((error) {
+//      print(error);
+//    });
+//
+//    if (imageFile == null) {
+//      return new Text('no data \n${widget.fileName}.jpg');
+//    } else {
+//      return new Image.memory(
+//        imageFile,
+//        width: 150,
+//        height: 150,
+//      );
+//    }
 
   @override
   Widget build(BuildContext context) {
     print('zzzz');
 
     const int MAXSIZE = 4 * 1024 * 1024;
-    cardImgReference
-        .child('${widget.fileName}.jpg')
-        .getData(MAXSIZE)
-        .then((data) {
-//      imageFile = data;
-      print(widget.fileName);
-      this.setState(() {
-        imageFile = data;
+    if (imgUrl == null) {
+      cardImgReference
+          .child('${widget.fileName}.jpg')
+          .getDownloadURL()
+          .then((data) {
+//
+////      print(widget.fileName);
+        this.setState(() {
+          print('nnnn');
+          imgUrl = data;
+        });
+      }).catchError((error) {
+        print(error);
       });
     }
-    ).catchError((error) {
-      print(error);
-    });
-    
-    if (imageFile == null) {
+    print(imgUrl);
+//    print(data);
+//    imgUrl=data;
+    if (imgUrl == null) {
       return new Text('no data \n${widget.fileName}.jpg');
     } else {
-      return new Image.memory(
-        imageFile,
+      print('ddd');
+      return Container(
+        child: CachedNetworkImage(
+          imageUrl: '${imgUrl}',
+          fit: BoxFit.fitWidth,
+        ),
         width: 150,
         height: 150,
       );
